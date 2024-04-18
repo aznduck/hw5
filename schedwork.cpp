8e0ba39b61ce2a24def0cd3c345840a9cdf7e9c0
David Han <dhan6663@usc.edu>
2024-04-18 00:51:30 +0000

    assignment 5 submission

--- schedwork.cpp
+++ schedwork.cpp
@@ -22,7 +22,8 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 // Add prototypes for any helper functions here
 
-
+bool search(size_t day, const AvailabilityMatrix& avail, const size_t dailyNeed, const size_t maxShifts, DailySchedule& sched, vector<size_t>& shiftsWorked, size_t worker);
+void addToSchedule(DailySchedule& sched, int row, int col, Worker_T id, vector<size_t>& shiftsWorked);
 // Add your implementation of schedule() and other helper functions here
 
 bool schedule(
@@ -36,10 +37,43 @@ bool schedule(
         return false;
     }
     sched.clear();
-    // Add your code below
+    vector<size_t> shiftsWorked(avail[0].size(), 0);
+    size_t day = 0;
+    size_t worker = 0;
+    sched.resize(avail.size());
+    if (search(day, avail, dailyNeed, maxShifts, sched, shiftsWorked, worker))
+      return true;
+    return false;
+}
 
-
-
-
+bool search(size_t day, const AvailabilityMatrix& avail, const size_t dailyNeed, const size_t maxShifts, DailySchedule& sched, vector<size_t>& shiftsWorked, size_t worker)
+{
+  if(day == avail.size()){
+    cout << "solved";
+    return true;
+  }
+  else
+  {
+    for(Worker_T w = worker; w < avail[0].size(); ++w)
+    {
+      if(avail[day][w] && shiftsWorked[w] < maxShifts)
+      {
+        sched[day].push_back(w);
+        shiftsWorked[w]++;
+        if (sched[day].size() == dailyNeed) {
+                if (search(day + 1, avail, dailyNeed, maxShifts, sched, shiftsWorked, 0)) {
+                    return true; 
+                }
+        } else {
+        if(search(day, avail, dailyNeed, maxShifts, sched, shiftsWorked, w + 1)){
+          return true;
+        }
+      }
+      sched[day].pop_back();
+      shiftsWorked[w]--;
+    }
+  }
+  return false;
+  }
 }
 
--- wordle.cpp
+++ wordle.cpp
@@ -5,6 +5,8 @@
 #include <algorithm> 
 #include <map>
 #include <set>
+#include <unordered_map>
+#include <vector>
 #endif
 
 #include "wordle.h"
@@ -12,17 +14,53 @@
 using namespace std;
 
 
-// Add prototypes of helper functions here
 
 
-// Definition of primary wordle function
+void wordHelp(const std::string& in, std::string& floating, const set<string>& dict, unsigned int n, string curr, std::set<std::string>& ans, int filled);
+
 std::set<std::string> wordle(
     const std::string& in,
     const std::string& floating,
     const std::set<std::string>& dict)
 {
-    // Add your code here
-
+    std::set<std::string> ans;
+    string floatingRef = floating;
+    int filled = in.size() - std::count(in.begin(), in.end(), '-');
+    wordHelp(in, floatingRef, dict, 0, in, ans, filled);
+    return ans;
 }
 
-// Define any helper functions here
+void wordHelp(const std::string& in, std::string& floating, const set<string>& dict, unsigned int n, string curr, std::set<std::string>& ans, int filled) {
+    if(floating.size() + filled > (in.size() - n))
+        return;
+    if (n == curr.size()) {
+        if (!(dict.find(curr) != dict.end())) 
+            return;
+        else if (floating.empty())
+        {
+            ans.insert(curr);
+            return;
+        }
+        return;    
+    }
+    
+    if (curr[n] != '-') {
+        wordHelp(in, floating, dict, n + 1, curr, ans, filled - 1);
+    } else {
+        for (char letter = 'a'; letter <= 'z'; ++letter) {
+            size_t pos = floating.find(letter);
+            if (pos != std::string::npos) {
+                string newFloating = floating;
+                newFloating.erase(pos, 1);
+                curr[n] = letter;
+                wordHelp(in, newFloating, dict, n + 1, curr, ans, filled);
+            }
+                curr[n] = letter;
+                wordHelp(in, floating, dict, n + 1, curr, ans, filled);
+        }
+    }
+}
+
+
+
+
